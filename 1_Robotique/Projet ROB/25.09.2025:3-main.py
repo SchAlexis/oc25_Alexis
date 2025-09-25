8d711bb05e5fdf64263d6db4384b6883ba220bd3
Elouan7 <elouan.delorme@icloud.com>
2025-09-25 14:58:59 +0200

    version 3
    ajouter un programme pour s'arreter quand il voit un objet et faire un 180°

---
+++ 1_Robotique/Projet ROB/25.09.2025:3-main.py
@@ -0,0 +1,70 @@
+from microbit import *
+from machine import time_pulse_us
+import KitronikMOVEMotor
+import random
+import neopixel
+
+trigger = pin13
+echo = pin14
+
+robot = KitronikMOVEMotor.MOVEMotor()
+robot.move(0, 0)
+
+trigger.write_digital(0)
+echo.read_digital()
+
+
+
+def distance_cm():
+    left = pin1.read_analog()
+    right = pin2.read_analog()
+    d = (left - right)
+    d = d // 10
+    robot.move(15 - d, 15 + d)
+    trigger.write_digital(1)
+    trigger.write_digital(0)
+    distance = time_pulse_us(echo, 1)/58.
+    return round(distance)
+
+
+
+
+while True:
+
+    d = distance_cm()
+    for i in range(25):
+        if i < d:
+            display.set_pixel(i//5, i%5, 9)
+        else:
+            display.set_pixel(i//5, i%5, 0)
+    
+    if d <= 10:
+        robot.move(0, 0, 1000)
+        robot.move(-60, -60, 500)#faire plus reculer
+        robot.goToPosition(1, 160)
+        robot.move(60, -60, 1300)
+        robot.move(-60, -60, 750)
+        display.show(Image('09090:'
+                           '09090:'
+                           '00000:'
+                           '90009:'
+                           '09990'))
+        #for i in range(4):
+            #np[i] = random.choice(colors)
+            #np.show()
+            #sleep(random.randint(100, 500))
+
+        #audio.play(Sound.SPRING)
+        robot.move(0, 0)
+        sleep(1000)
+        robot.goToPosition(1, 20)
+        robot.move(60, -60, 700)
+        sleep(1000)
+        robot.goToPosition(1, 160)
+        sleep(1000)
+        robot.move(-60, 60, 700)
+        robot.move(60, -60, 1300)
+        robot.goToPosition(1, 20)
+        
+        
+        
