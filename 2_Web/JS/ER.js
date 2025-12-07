4e941d20723c5f7309a58a08ca61764d7bdb1a37
SchAlexis <schmidalexis08@gmail.com>
2025-12-07 20:48:16 +0100

    final

--- 2_Web/JS/ER.js
+++ 2_Web/JS/ER.js
@@ -34,7 +34,7 @@ const img = new Image();
 img.src = "images/horse.svg";
 
 let angle = 0;
-let direction = 1; // 1 = va vers 5° ; -1 = revient vers -5°
+let direction = 1;
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -42,22 +42,17 @@ function draw() {
     // Déterminer l'angle d'oscillation
     angle += 0.02 * direction;
 
-    if (angle > 0.2) direction = -1;   // limite haute (~8.6°)
-    if (angle < -0.2) direction = 1;   // limite basse (~-8.6°)
+    if (angle > 0.2) direction = -1;   // limite haute en ° de l'angle
+    if (angle < -0.2) direction = 1;   // limite basse en ° de l'angle 
 
-    // Sauvegarde du contexte
     ctx.save();
 
-    // Déplacer le point de rotation au centre de l'image
     ctx.translate(100, 100);
 
-    // Appliquer rotation
     ctx.rotate(angle);
 
-    // Dessiner l'image centrée
     ctx.drawImage(img, -65, -70, 100, 100);
 
-    // Restaurer
     ctx.restore();
 
     requestAnimationFrame(draw);
